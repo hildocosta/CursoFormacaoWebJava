1b4bc4ca9cb8f90eaae7f9a8ebb06de4491197d1
USER <USER@DESKTOP-IICEJ5A>
2023-03-14 20:30:13 -0300

    Aula 4 - O Tipo CHAR de caracteres

---
+++ CursoFormacaoWebJavaAula4/src/cursojava/executavel/Aula4.java
@@ -0,0 +1,27 @@
+package cursojava.executavel;
+
+public class Aula4 {
+	
+/* Título da Aula 4: O Tipo CHAR de caracteres*/
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+	/* CHAR representa uma letra*/
+		
+	char pessoaFeminino = 'F';
+	
+	if (pessoaFeminino == 'F') {
+		
+		System.out.println("Feminino");
+		
+	} else {
+		
+		System.out.println("Masculino");
+	}
+	
+	
+	
+	}
+
+}
