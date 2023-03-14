f0cb97062c758f0f86365a8da5ece06c11bb8bb5
USER <USER@DESKTOP-IICEJ5A>
2023-03-14 20:31:16 -0300

    Aula 5 - O Tipo String de caracteres

---
+++ CursoFormacaoWebJavaAula5/src/cursojava/executavel/Aula5.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+public class Aula5 {
+	
+/* Título da Aula 5: O Tipo String de Caracteres*/
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+	/*String no Java sempre será texto*/
+		
+		String nome ;
+		String cpf;
+		String telefone;
+		String endereco;
+	
+	
+	}
+
+}
