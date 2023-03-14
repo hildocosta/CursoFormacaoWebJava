0faf9400bcf852209f8183d551fa26a9dc36ad46
USER <USER@DESKTOP-IICEJ5A>
2023-03-14 20:32:40 -0300

    Aula 6 - Concatenação de variáveis

---
+++ CursoFormacaoWebJavaAula6/src/cursojava/executavel/Aula6.java
@@ -0,0 +1,23 @@
+package cursojava.executavel;
+
+public class Aula6 {
+	
+	
+/* Título da Aula 6: Concatenação de Variáveis */
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+/*Concatenação = Unir ou Juntar dados*/
+		
+		String nome = "Hildo";
+		String cpf = "357.975.638-94";
+		String telefone = "(41) 98470-8882" ;
+		String endereco = "Rua Pará, São José dos Pinhais - PR";
+		
+	 System.out.println("Meu nome é :" + nome + " e o CPF é :"+ cpf + " meu contato é : " + telefone + " e eu moro em : " + endereco);	
+	  
+	
+	}
+
+}
