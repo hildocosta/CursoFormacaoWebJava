bfd21d712569b8b8ed30c9e3ca473a32779d6517
USER <USER@DESKTOP-IICEJ5A>
2023-03-14 20:28:32 -0300

    Aula 3 - Tipo Double de dados

---
+++ CursoFormacaoWebJavaAula3/src/cursojava/executavel/Aula3.java
@@ -0,0 +1,24 @@
+package cursojava.executavel;
+
+public class Aula3 {
+
+/* Título da Aula 3: Tipo Double de dados*/
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*variáveis*/
+		double nota1 = 90;
+	    double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		/*Double aceita casa decimal*/
+		
+		double mediaFinal = (nota1+nota2+nota3+nota4)/4;
+		
+		/*Imprime texto no console*/ 
+		System.out.println("Média Final é = " + (mediaFinal));
+
+}
+}
