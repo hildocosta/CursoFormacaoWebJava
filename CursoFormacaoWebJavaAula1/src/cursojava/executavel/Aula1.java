ad4cd356b7dea19038bb18f85c5657cacf00b6ac
USER <USER@DESKTOP-IICEJ5A>
2023-03-14 20:25:52 -0300

    Aula 1 - Criando o primeiro projeto Java

---
+++ CursoFormacaoWebJavaAula1/src/cursojava/executavel/Aula1.java
@@ -0,0 +1,17 @@
+package cursojava.executavel;
+
+public class Aula1 {
+	
+/* Título da Aula 1: Primeiro Projeto Java*/
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		
+		/*Imprime texto no console*/ 
+		System.out.println("Meu primeiro projeto Java ");
+		System.out.println("Java é orientado a objeto ");
+		
+	}
+
+}
