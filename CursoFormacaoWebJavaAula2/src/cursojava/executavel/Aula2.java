b58dba0383603a888dd040dc7e4f25712dc0ab25
USER <USER@DESKTOP-IICEJ5A>
2023-03-14 20:27:23 -0300

    Aula 2 - Operações entre números inteiros

---
+++ CursoFormacaoWebJavaAula2/src/cursojava/executavel/Aula2.java
@@ -0,0 +1,24 @@
+package cursojava.executavel;
+
+public class Aula2 {
+	
+/* Título da Aula 2: Operações entre Números Inteiros*/
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*variáveis*/
+		int nota1 = 90;
+	    int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+			
+		int mediaFinal = (nota1+nota2+nota3+nota4)/4;
+		
+		/*Imprime texto no console*/ 
+		System.out.println("Média Final é = " + (mediaFinal));
+		
+	}
+
+}
