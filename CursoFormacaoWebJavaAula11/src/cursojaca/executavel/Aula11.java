235c1c81ce3c2b399ecbcd2ad35f3fb1955447bf
USER <USER@DESKTOP-IICEJ5A>
2023-03-18 12:12:13 -0300

    Aula 11 - Operadores Relacionais

---
+++ CursoFormacaoWebJavaAula11/src/cursojaca/executavel/Aula11.java
@@ -0,0 +1,34 @@
+package cursojaca.executavel;
+
+public class Aula11 {
+	
+	/* Título da Aula 11: Operadores Relacionais */
+	
+	/*
+	 * 
+	 *  == significa Igual
+	 *  != significa Diferente
+	 *  <  significa Menor
+	 *  >  significa Maior
+	 *  <= significa Menor Igual
+	 *  >= significa Maior Igual
+	 *  
+	 *  
+	 */
+	
+
+	public static void main(String[] args) {
+		
+		int nota1 = 91;
+		int nota2 = 90;
+		
+		if (nota1 == nota2) {
+			System.out.println(" As notas são iguais");
+		}
+		else {
+			System.out.println(" As notas são diferentes");
+		}
+
+
+}
+}
