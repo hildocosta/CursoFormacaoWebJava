c8a8090578a8c323d56b0670adc16c4a4afde043
USER <USER@DESKTOP-IICEJ5A>
2023-03-20 08:33:25 -0300

    Aula 12 - Estrutura de Repetição While e Do While

---
+++ CursoFormacaoWebJavaAula12/src/cursojava/executavel/Aula12.java
@@ -0,0 +1,43 @@
+package cursojava.executavel;
+
+public class Aula12 {
+	
+	/* Título da Aula 12: Estrutura de Repetição While e Do While */
+
+	
+	public static void main(String[] args) {
+		
+	/*Estrutura de repetição While*/
+		
+		int numero = 0;
+		
+		/* While = verifica e depois executa*/
+		while (numero <= 10) {
+			
+			System.out.println ("O número atual é: " + numero);
+			numero ++;
+			
+		}
+		
+		
+		/*Exemplo com Do While*/
+		
+		
+		int numero2 = 0;
+		
+		do /*Do While = executa e depois verifica */
+			
+			System.out.println ("O número atual é " + numero2);
+		numero2 ++;
+		
+		while (numero2 <=10);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
