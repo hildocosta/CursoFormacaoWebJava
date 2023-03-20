28ee0319d60fe8db03b37edad53e2ec3c06222ad
USER <USER@DESKTOP-IICEJ5A>
2023-03-20 11:50:41 -0300

    Aula 14 : Estrutura de Repetição For e Parada com Break

---
+++ CursoFormacaoWebJavaAula14/src/cursojava/executavel/Aula14.java
@@ -0,0 +1,19 @@
+package cursojava.executavel;
+
+public class Aula14 {
+	
+/* Título da Aula 13: Estrutura de Repetição For e Parada com Break */
+
+	
+	public static void main(String[] args) {
+		
+		for (int numero = 0; numero <= 10; numero ++ ) {
+			if (numero == 10) {
+				System.out.println("Oba, encontrei o número 10");
+				System.out.println("Estou parando de executar...");
+				break;
+			}
+	}
+		
+	}
+}
