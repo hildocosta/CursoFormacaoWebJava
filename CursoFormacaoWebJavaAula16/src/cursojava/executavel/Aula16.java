b226eabc7d8c665dc4a4a9bac777f8eb31511568
USER <USER@DESKTOP-IICEJ5A>
2023-03-20 12:16:17 -0300

    Aula 16 -  Divisões e resto com MOD do Java

---
+++ CursoFormacaoWebJavaAula16/src/cursojava/executavel/Aula16.java
@@ -0,0 +1,18 @@
+package cursojava.executavel;
+
+public class Aula16 {
+	
+	/* Título da Aula 16: Divisões e resto com MOD do Java */
+	/* Módulo: É o resto da divisão % */
+	
+	public static void main(String[] args) {
+		
+		double carro = 9;
+		double pessoa = 2;
+		
+		double resto = carro % pessoa;
+		
+		System.out.println("Sobraram exatamente : " + resto + " carros.");
+
+}
+}
