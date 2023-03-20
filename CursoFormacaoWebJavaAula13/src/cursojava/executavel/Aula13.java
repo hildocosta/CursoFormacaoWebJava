7c053256c5b423d3249f62fb7eaa0a0ddd2bba4a
USER <USER@DESKTOP-IICEJ5A>
2023-03-20 11:15:32 -0300

    Aula 13 - Estrutura de Repetição For

---
+++ CursoFormacaoWebJavaAula13/src/cursojava/executavel/Aula13.java
@@ -0,0 +1,22 @@
+package cursojava.executavel;
+
+public class Aula13 {
+	
+/* Título da Aula 13: Estrutura de Repetição For */
+
+	
+	public static void main(String[] args) {
+		
+	/*Estrutura de repetição For */
+		
+    /* numero ++: serve para quebrar a repetição, em ordem crescente */
+	/* numero --: serve para quebrar a repetiçào, em ordem decrescente */
+		
+		for (int numero = 0; numero <= 10; numero ++ ) {
+			System.out.println("O número atual é: " + numero);
+		}
+			
+		
+		}
+
+}
