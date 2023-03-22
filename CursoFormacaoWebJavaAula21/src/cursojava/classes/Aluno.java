08c6ada84f4862ca8ba335368100d0c091892ebe
USER <USER@DESKTOP-IICEJ5A>
2023-03-22 15:07:30 -0300

    Aula 21 - Sobre o Construtor de Objeto

---
+++ CursoFormacaoWebJavaAula21/src/cursojava/classes/Aluno.java
@@ -0,0 +1,35 @@
+package cursojava.classes;
+
+/* Está é a nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+	
+	
+/*Aula 20: Criando a Primeira Classe e Objeto "Aluno" */
+		
+		String nome;
+		int idade;
+		String dataNascimento;
+		String registroGeral;
+		String numeroCpf;
+		String nomeMae;
+		String nomePai;
+		String dataMatricula;
+		String nomeEscola;
+		String serieMatriculado;
+		
+		//Construtor do Java
+		public Aluno() {
+			// Cria os dados na memória - Sendo padrão do Java
+			// TODO Auto-generated constructor stub
+		}
+		
+		public Aluno(String nomePadrao) {
+			nome = nomePadrao;
+		}
+		
+	}
+	
+	
+
+
+
---
+++ CursoFormacaoWebJavaAula21/src/cursojava/executavel/Aula21.java
@@ -0,0 +1,23 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class Aula21 {
+	
+/*Aula 21: Sobre o Construtor de Objeto */
+	
+	public static void main(String[] args) {
+		
+		/* Comando new: Agora temos um objeto real na memória, preparado para execução*/
+		// Instaciando o Objeto
+		
+		Aluno aluno1 = new Aluno(); /*Aqui será o Aluno João*/
+		
+		Aluno aluno2 = new Aluno(); // Aqui será o Pedro
+		
+		Aluno aluno3 = new Aluno(); // Aqui será o Alex
+		
+		Aluno aluno4 = new Aluno("Maria");
+
+}
+}
