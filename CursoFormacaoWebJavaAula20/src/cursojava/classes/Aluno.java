b674cb2a760a35115d1d21b693276686361c27a6
USER <USER@DESKTOP-IICEJ5A>
2023-03-22 15:06:39 -0300

    Aula 20 - Criando a Primeira Classe e Objeto "Aluno"

---
+++ CursoFormacaoWebJavaAula20/src/cursojava/classes/Aluno.java
@@ -0,0 +1,21 @@
+package cursojava.classes;
+
+/* Está é a nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+	
+	/*Aula 20: Criando a Primeira Classe e Objeto "Aluno" */
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	
+
+}
---
+++ CursoFormacaoWebJavaAula20/src/cursojava/executavel/Aula20.java
@@ -0,0 +1,24 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class Aula20 {
+	
+	/*Aula 20: Criando a Primeira Classe e Objeto "Aluno" */
+	
+	public static void main(String [] args) {
+		
+		
+		Aluno aluno1 = new Aluno(); //Aqui sera o João
+		
+		Aluno aluno2 = new Aluno(); /*Aqui será o Aluno Pedro*/
+		
+		Aluno aluno3 = new Aluno(); /*Aqui será o Aluno Alex*/
+		
+		
+		
+		
+		
+	}
+
+}
